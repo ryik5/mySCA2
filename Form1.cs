55c06a52317133aed23d148ab287d8bedd3fc47d
ryik5 <ryik.yuri@gmail.com>
2019-01-10 23:50:24 +0200

    check some data

--- Form1.cs
+++ Form1.cs
@@ -6799,10 +6799,21 @@ namespace PersonViewerSCA2
 
     public static class DataTableExtensions
     {
-        public static void SetColumnsOrder(this DataTable table, params String[] columnNames)
+        public static void SetColumnsOrder(this DataTable table, params string[] columnNames)
         {
+            List<string> listColNames = columnNames.ToList();
+
+            //Remove invalid column names.
+            foreach (string colName in columnNames)
+            {
+                if (!table.Columns.Contains(colName))
+                {
+                    listColNames.Remove(colName);
+                }
+            }
+
             int columnIndex = 0;
-            foreach (var columnName in columnNames)
+            foreach (var columnName in listColNames)
             {
                 table.Columns[columnName].SetOrdinal(columnIndex);
                 columnIndex++;
