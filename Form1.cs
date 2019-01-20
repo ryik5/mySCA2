dca2ccafba7a7aa25092c9ccafe078831f30a72c
ryik5 <ryik.yuri@gmail.com>
2019-01-20 20:12:15 +0200

    q

--- Form1.cs
+++ Form1.cs
@@ -1220,12 +1220,12 @@ namespace PersonViewerSCA2
                             {
                                 while (reader.Read())
                                 {
-                                    if (reader?.GetString(@"code") != null && reader?.GetString(@"code").Length > 0 && reader?.GetString(@"start_date") != null && reader?.GetString(@"start_date").Length > 0)
+                                    if (reader?.GetString(@"code") != null && reader?.GetString(@"code").Length > 0 )
                                     {
                                         peopleShifts.Add(new PeopleShift()
                                         {
                                             _nav = reader.GetString(@"code"),
-                                            _dayStartShift = reader?.GetString(@"start_date"),
+                                          //  _dayStartShift = reader?.GetString(@"start_date"),
                                             _MoStart = Convert.ToInt32(reader.GetString(@"mo_start")),
                                             _MoEnd = Convert.ToInt32(reader.GetString(@"mo_end")),
                                             _TuStart = Convert.ToInt32(reader.GetString(@"tu_start")),
