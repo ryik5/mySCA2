731085ed154ba74cc1201a4228ddea983e4f298d
ryik5 <ryik.yuri@gmail.com>
2019-04-05 16:48:55 +0300

    added two classes Timer and TimeConvertor

--- AdditionalClasses.cs
+++ AdditionalClasses.cs
@@ -79,6 +79,7 @@ label1.Text = name;
 }
 */
 
+
     class ParameterConfig
     {
         public string parameterName;
@@ -800,6 +801,50 @@ label1.Text = name;
                 table.Columns[columnName].SetOrdinal(columnIndex);
                 columnIndex++;
             }
+        }
+    }
+
+    //todo
+    //replace using:
+    /*
+        TimeConvertor timeConvertor1 = new TimeConvertor { Seconds = 115 };
+        Timer timer = timeConvertor1;
+        Console.WriteLine($"{timer.Hours}:{timer.Minutes}:{timer.Seconds}"); // 0:1:55
+ 
+        TimeConvertor timeConvertor2 = (TimeConvertor)timer;
+        Console.WriteLine(timeConvertor2.Seconds);  //115
+        */
+    class Timer
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+    }
+
+    class TimeConvertor
+    {
+        public int Seconds { get; set; }
+
+        public static implicit operator TimeConvertor(int x)
+        {
+            return new TimeConvertor { Seconds = x };
+        }
+        public static explicit operator int(TimeConvertor timeConvertor)
+        {
+            return timeConvertor.Seconds;
+        }
+        public static explicit operator TimeConvertor(Timer timer)
+        {
+            int h = timer.Hours * 3600;
+            int m = timer.Minutes * 60;
+            return new TimeConvertor { Seconds = h + m + timer.Seconds };
+        }
+        public static implicit operator Timer(TimeConvertor timeConvertor)
+        {
+            int h = timeConvertor.Seconds / 3600;
+            int m = (timeConvertor.Seconds - h * 3600) / 60;
+            int s = timeConvertor.Seconds - h * 3600 - m * 60;
+            return new Timer { Hours = h, Minutes = m, Seconds = s };
         }
     }
 
