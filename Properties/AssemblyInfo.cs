87ccfb887b425acbc0bd8678973fcfe7a260e2f2
ryik5 <ryik.yuri@gmail.com>
2019-07-22 11:24:34 +0300

    bug fixing

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //   Номер сборки
 //      Редакция
 //
-[assembly: AssemblyVersion("3.9.5.59")]
-[assembly: AssemblyFileVersion("3.7.11.59")]
+[assembly: AssemblyVersion("3.9.5.61")]
+[assembly: AssemblyFileVersion("3.7.11.60")]
